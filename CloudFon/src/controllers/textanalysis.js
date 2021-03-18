ac1de01993a6e35dfc0027a92754d8edf37a546b
FrankieDLT <51407226+FrankieDLT@users.noreply.github.com>
2021-03-18 15:09:06 -0600

    Sexto commit
    Después de un descanso, se actualizó para incluir el Tone Analyzer, el cual funciona hasta cierto punto, pues es necesario recargar la pagina para obtener el nuevo análisis del texto.

--- CloudFon/src/controllers/textanalysis.js
+++ CloudFon/src/controllers/textanalysis.js
@@ -1,14 +1,49 @@
+require('dotenv').config();
+
+var fintext;
+
+const ToneAnalyzerV3 = require('ibm-watson/tone-analyzer/v3');
+const {
+    IamAuthenticator
+} = require('ibm-watson/auth');
+
+const toneAnalyzer = new ToneAnalyzerV3({
+    version: '2017-09-21',
+    authenticator: new IamAuthenticator({
+        apikey: process.env.API_KEY,
+    }),
+    serviceUrl: 'https://api.us-east.tone-analyzer.watson.cloud.ibm.com',
+    disableSslVerification: true,
+});
+
 class ibmCloudF {
     TextAn(req, res) {
-        const {
-            frase
-        } = req.body;
         
-       
-        res.send(req.body.text);
+        const toneParams = {
+            toneInput: {
+                'text': req.body.text
+            },
+            contentType: 'application/json',
+        };
+
+        toneAnalyzer.tone(toneParams)
+            .then(toneAnalysis => {
+                console.log(JSON.stringify(toneAnalysis, null, 2));
+                fintext = JSON.stringify(toneAnalysis, null, 2);
+                fintext = JSON.parse(fintext);
+                console.log("Emoción principal: " + fintext.result.document_tone.tones[0].tone_name);
+                fintext = "Emoción principal: " + fintext.result.document_tone.tones[0].tone_name;
+            })
+            .catch(err => {
+                console.log('error:', err);
+                fintext = 'Tenemos un error: ' + err;
+            });
+
+
+        res.send(fintext);
     }
 
-    
+
 }
 
 module.exports = new ibmCloudF();
